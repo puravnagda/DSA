ead8d9c66c49eec69d5a1445c9eb4fece36cdf98
Purav Nagda <34997868+puravnagda@users.noreply.github.com>
2022-12-25 14:21:32 +0530

    Added solution - LeetHub

---
+++ Searching an element in a sorted array - GFG/searching-an-element-in-a-sorted-array.java
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class gfg
+{
+    
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0)
+        {
+            String s[] = read.readLine().trim().split("\\s+");
+            
+            int n = Integer.parseInt(s[0]);
+            int k = Integer.parseInt(s[1]);
+            
+            int arr[] = new int[n];
+            
+            String st[] = read.readLine().trim().split("\\s+");
+            
+            for(int i = 0; i < n; i++)
+            {
+                arr[i] = Integer.parseInt(st[i]);
+            }
+            
+            Solution obj = new Solution();
+            
+            System.out.println(obj.searchInSorted(arr, n, k));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution{
+    static int searchInSorted(int arr[], int N, int K)
+    {
+        return binarySearch(arr,K,0,N-1);
+    }
+    
+    static int binarySearch(int arr[], int x, int low, int high){
+        while(low <= high){
+            int mid = (high - low) + low / 2;
+            if(x == arr[mid])
+                return 1;
+            if(x > arr[mid])
+                low+=1;
+            else
+                high-=1;
+        }
+        return -1;
+    }
+}
