658d4264a7e85cc5159bb5904f9a2174bc646046
Purav Nagda <pnagda426@gmail.com>
2024-04-23 08:57:54 +0530

    Restructing Leethub

---
+++ Leethub/max-consecutive-ones/max-consecutive-ones.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+       int count = 0, max = 0;
+        if(nums.length == 0)
+            return max;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] == 1)
+                count ++;
+            else
+                count = 0;
+            if(count > max)
+                max = count;
+        }
+        return max;
+    }
+}
